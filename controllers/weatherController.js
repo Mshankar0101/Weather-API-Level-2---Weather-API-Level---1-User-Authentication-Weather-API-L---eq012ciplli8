0e1c9808b0c7de3e868ddafcc8e176633f779a51
Mshankar0101 <144152894+Mshankar0101@users.noreply.github.com>
2024-03-24 15:03:54 +0530

    Update weatherController.js

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -10,7 +10,7 @@ const getWeatherByCityName = async (req, res) => {
     // Example response when data is not found:
     // res.status(404).json({ message: 'Weather data not found for the given city' });
     const weatherData = JSON.parse(fs.readFileSync(`./data/data.json`))
-    const dataByCityName = weatherData.find((el)=> el.cityName.toLowerCase() === cityName.toLowerCase());
+    const dataByCityName = weatherData.find((el)=> el.cityName.toLowerCase().trim() === cityName.toLowerCase().trim());
     if(dataByCityName){
       return res.status(200).json({"weatherData": dataByCityName});
     }
