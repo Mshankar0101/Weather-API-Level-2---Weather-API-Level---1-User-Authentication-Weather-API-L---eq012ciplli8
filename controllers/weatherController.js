0b2ed2a1009336f16edb295d58b64b7780d8ff62
Mshankar0101 <144152894+Mshankar0101@users.noreply.github.com>
2024-03-26 20:54:44 +0530

    Update weatherController.js

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,25 +1,47 @@
 const WeatherData = require('../models/weatherDataModel');
 const fs = require("fs");
 const getWeatherByCityName = async (req, res) => {
-  const cityName = req.params.cityName;
+const cityName = req.params.cityName;
+try {
+// TODO: Implement logic to retrieve weather data by city name
+// Example response when data is found:
+// res.status(200).json({ weatherData });
+// Example response when data is not found:
+// res.status(404).json({ message: 'Weather data not found for the given city' });
+const cityData = await WeatherData.findOne({ cityName });
+if (cityData) {
+res.status(200).json({ weatherData: cityData });
+} else {
+res.status(404).send({
+message: "Weather data not found for the given city",
+});
+}
+} catch (error) {
+res.status(500).json({
+message: "Internal server error",
+error: error.message,
+});
+}
 
-  try {
-    // TODO: Implement logic to retrieve weather data by city name
-    // Example response when data is found:
-    // res.status(200).json({ weatherData });
-    // Example response when data is not found:
-    // res.status(404).json({ message: 'Weather data not found for the given city' });
-    const weatherData = JSON.parse(fs.readFileSync(`./data/data.json`))
-    const dataByCityName = weatherData.find((el)=> el.cityName.toLowerCase().trim() === cityName.toLowerCase().trim());
-    if(dataByCityName){
-      return res.status(200).json({"weatherData": dataByCityName});
-    }
-    return res.status(404).json({ message: 'Weather data not found for the given city' });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal server error', error: error.message });
-  }
+  
+  // const cityName = req.params.cityName;
+  // try {
+  //   // TODO: Implement logic to retrieve weather data by city name
+  //   // Example response when data is found:
+  //   // res.status(200).json({ weatherData });
+  //   // Example response when data is not found:
+  //   // res.status(404).json({ message: 'Weather data not found for the given city' });
+  //   const weatherData = JSON.parse(fs.readFileSync(`./data/data.json`))
+  //   const dataByCityName = weatherData.find((el)=> el.cityName.toLowerCase().trim() === cityName.toLowerCase().trim());
+  //   if(dataByCityName){
+  //     return res.status(200).json({"weatherData": dataByCityName});
+  //   }
+  //   return res.status(404).json({ message: 'Weather data not found for the given city' });
+  // } catch (error) {
+  //   res
+  //     .status(500)
+  //     .json({ message: 'Internal server error', error: error.message });
+  // }
 };
 
 const getWeatherByZipCode = async (req, res) => {
